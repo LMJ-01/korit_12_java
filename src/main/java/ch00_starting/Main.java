b75d419bf82ed188ebc7a917a61a3e560b43043e
LMJ-01 <dlaudwns0903@gmail.com>
2025-12-22 17:01:48 +0900

    operator lessoning

--- src/main/java/ch00_starting/Main.java
+++ src/main/java/ch00_starting/Main.java
@@ -2,6 +2,14 @@ package ch00_starting;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Java!");
+        System.out.println("ctrl + d : 현재 줄 복사");
+        System.out.println("shift + f10 : 실행");
+        System.out.println("ctrl + / : 주석처리");
+        System.out.println("이명준");
+        System.out.println("123");
+        System.out.println("12");
+//        System.out.println(123456);
+//        System.out.println(hello);_
+        // ch01_variable 패키지를 만들고 Main 클래스를 생성하시오.
     }
 }
